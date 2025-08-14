f695a7f32f9d4e52bf31840da18a9949beb88973
guiqiqi <guiqiqi187@gmail.com>
2025-08-14 10:42:27 +0800

    Change default command port to 80

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     command_server: str = '192.168.50.31'
-    command_port: int = 8080
+    command_port: int = 80
     measure_interval: int = 5
     database: str = 'database.db'
     listen: str = '127.0.0.1'
